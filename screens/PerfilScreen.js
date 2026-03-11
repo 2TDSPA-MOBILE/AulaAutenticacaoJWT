5949db8a37059858224aeae451460ae93cfcd6d0
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2026-03-11 20:14:55 -0300

    Implementado o logout(limpeza do async storagre

--- screens/PerfilScreen.js
+++ screens/PerfilScreen.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react"
-import { View, Text } from "react-native"
+import { View, Text, Button } from "react-native"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import api from "../services/api"
 
-export default function PerfilScreen(){
+export default function PerfilScreen({ navigation }){
 
  const [user, setUser] = useState(null)
 
+ // Função que busca os dados do usuário na API
  async function carregarPerfil(){
 
+   // Recupera o token salvo no celular
    const token = await AsyncStorage.getItem("token")
 
+   // Faz requisição para rota protegida
    const response = await api.get("/perfil", {
      headers:{
        Authorization: `Bearer ${token}`
      }
    })
 
+   // Salva dados do usuário no estado
    setUser(response.data.user)
 
  }
@@ -24,6 +28,17 @@ export default function PerfilScreen(){
  useEffect(()=>{
    carregarPerfil()
  },[])
+
+ // FUNÇÃO DE LOGOUT
+ async function logout(){
+
+   // Remove o token do armazenamento do celular
+   await AsyncStorage.removeItem("token")
+
+   // Volta para tela de login
+   navigation.navigate("Login")
+
+ }
 
  return(
 
@@ -34,6 +49,15 @@ export default function PerfilScreen(){
      {user && (
        <Text>{user.email}</Text>
      )}
+
+     <View style={{marginTop:20}}>
+
+       <Button
+        title="Logout"
+        onPress={logout}
+       />
+
+     </View>
 
    </View>
 
